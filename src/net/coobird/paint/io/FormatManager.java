7326afd59aa0fedbbbe0389e02f2743d58d64199
Chris Kroells <coobird@coobird.net>
2009-02-05 15:55:37 +0000

    Began implementing FormatManager. Adding JavaSupportedImageOutput.

--- src/net/coobird/paint/io/FormatManager.java
+++ src/net/coobird/paint/io/FormatManager.java
@@ -1,9 +1,62 @@
 package net.coobird.paint.io;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author coobird
+ *
+ */
 public class FormatManager
 {
 	/**
 	 * Manage the supported formats
+	 * 
+	 * What is this class really for?
+	 * Perhaps tell it a desired format and return an approriate I/O filter?
 	 */
-
+	
+	private static List<ImageInput> inputList;
+	private static List<ImageOutput> outputList;
+	
+	static
+	{
+		inputList = new ArrayList<ImageInput>();
+		outputList = new ArrayList<ImageOutput>();
+		
+		inputList.add(new DefaultImageInput());
+		inputList.add(new JavaSupportedImageInput());
+		
+		outputList.add(new DefaultImageOutput());
+	}
+	
+	/**
+	 * Returns an {@link ImageInput} object which can handle the file format of
+	 * {@link File} object.
+	 * @param f				{@code File} object for which to find an
+	 * 						{@code ImageInput} for.
+	 * @return				The {@code ImageInput} which can read the given
+	 * 						file, or {@code null} if an adaquate
+	 * 						{@code ImageInput} cannot be found.
+	 */
+	public static ImageInput getImageInput(File f)
+	{
+		for (ImageInput imgInput : inputList)
+		{
+			if (imgInput.supportsFile(f))
+			{
+				return imgInput;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static ImageOutput getImageOutput(File f)
+	{
+		//TODO implement!
+		return null;		
+	}
 }
---
+++ src/net/coobird/paint/io/JavaSupportedImageOutput.java
@@ -0,0 +1,21 @@
+package net.coobird.paint.io;
+
+import java.io.File;
+
+import net.coobird.paint.image.Canvas;
+
+/**
+ * 
+ * @author coobird
+ *
+ */
+public class JavaSupportedImageOutput extends ImageOutput
+{
+
+	@Override
+	public void write(Canvas c, File f)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
