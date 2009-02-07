a5f90ce3a3008fcc5d58322fd9e728d7f6485cdd
Chris Kroells <coobird@coobird.net>
2009-02-07 15:14:03 +0000

    Added a way to store the application's main Component.

--- src/net/coobird/paint/application/ApplicationUtils.java
+++ src/net/coobird/paint/application/ApplicationUtils.java
@@ -1,15 +1,30 @@
 package net.coobird.paint.application;
 
+import java.awt.Component;
+
 import javax.swing.JOptionPane;
 
 public class ApplicationUtils
 {
+	private static Component mainComponent = null;
+	
 	public static void showExceptionMessage(Exception e)
 	{
-		String s = "There was an Exception.\n" + e.toString() + "\n" + e.getMessage();
+		String msg = "An exception has occurred.\n\n" + e.toString() +
+				"\n\nIf problems persist, please contact your local administrator.";
 		// log exception
 		e.printStackTrace(System.out);
 		
-		JOptionPane.showMessageDialog(null, s, e.getClass().toString(), JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(
+				mainComponent,
+				msg,
+				"Exception",
+				JOptionPane.ERROR_MESSAGE
+		);
+	}
+	
+	public static void setMainComponent(Component c)
+	{
+		mainComponent = c;
 	}
 }
