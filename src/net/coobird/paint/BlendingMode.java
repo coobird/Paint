8a0e7f7d92965d94568e312fede27ceb9259674d
Chris Kroells <coobird@coobird.net>
2009-02-27 03:54:18 +0000

    Style clean up and added javadocs.

--- src/net/coobird/paint/BlendingMode.java
+++ src/net/coobird/paint/BlendingMode.java
@@ -17,22 +17,74 @@ public class BlendingMode
 	 */
 	public enum Layer
 	{
+		/**
+		 * Normal blending mode for rendering {@link ImageLayer} objects.
+		 */
 		NORMAL (AlphaComposite.SrcOver),
+		
+		/**
+		 * Blending mode using {@link AlphaComposite.SrcOver} for rendering
+		 * {@link ImageLayer} objects.
+		 * Expected only for temporary use during development.
+		 */
 		SRC_OVER (AlphaComposite.SrcOver),
+		
+		/**
+		 * Blending mode using {@link AlphaComposite.SrcIn} for rendering
+		 * {@link ImageLayer} objects.
+		 * Expected only for temporary use during development.
+		 */
 		SRC_IN (AlphaComposite.SrcIn),
+		
+		/**
+		 * Blending mode using {@link AlphaComposite.SrcOut} for rendering
+		 * {@link ImageLayer} objects.
+		 * Expected only for temporary use during development.
+		 */
 		SRC_OUT (AlphaComposite.SrcOut),
+		
+		/**
+		 * Blending mode using {@link AlphaComposite.DstOver} for rendering
+		 * {@link ImageLayer} objects.
+		 * Expected only for temporary use during development.
+		 */
 		DST_OVER (AlphaComposite.DstOver),
+		
+		/**
+		 * Blending mode using {@link AlphaComposite.DstIn} for rendering
+		 * {@link ImageLayer} objects.
+		 * Expected only for temporary use during development.
+		 */
 		DST_IN (AlphaComposite.DstIn),
+		
+		/**
+		 * Blending mode using {@link AlphaComposite.DstOut} for rendering
+		 * {@link ImageLayer} objects.
+		 * Expected only for temporary use during development.
+		 */
 		DST_OUT (AlphaComposite.DstOut)
 		;
 		
+		/**
+		 * {@link AlphaComposite} used for the {@link ImageLayer}.
+		 */
 		private AlphaComposite ac;
 		
-		private Layer(AlphaComposite ac)
+		/**
+		 * Instantiates an {@code enum} for the {@link ImageLayer} with the
+		 * specified {@link AlphaComposite} object.
+		 * @param ac		The {@code AlphaComposite} for this enumeration.
+		 */		private Layer(AlphaComposite ac)
 		{
 			this.ac = ac;
 		}
 		
+		/**
+		 * Returns an {@link AlphaComposite} object associated with this
+		 * enumeration.
+		 * @return			An {@code AlphaComposite} object associated with
+		 * 					this enumeration.
+		 */
 		public AlphaComposite getComposite()
 		{
 			return ac;
@@ -46,17 +98,38 @@ public class BlendingMode
 	 */
 	public enum Brush
 	{
+		/**
+		 * Normal blending mode for rendering {@link Brush} objects.
+		 */
 		NORMAL (AlphaComposite.SrcOver),
+		
+		/**
+		 * Blending mode for rendering a {@link Brush} objects as an eraser.
+		 */
 		ERASER (AlphaComposite.DstOut)
 		;
 		
+		/**
+		 * {@link AlphaComposite} used for the {@link Brush}.
+		 */
 		private AlphaComposite ac;
 		
+		/**
+		 * Instantiates an {@code enum} for the {@link Brush} with the
+		 * specified {@link AlphaComposite} object.
+		 * @param ac		The {@code AlphaComposite} for this enumeration.
+		 */
 		private Brush(AlphaComposite ac)
 		{
 			this.ac = ac;
 		}
 		
+		/**
+		 * Returns an {@link AlphaComposite} object associated with this
+		 * enumeration.
+		 * @return			An {@code AlphaComposite} object associated with
+		 * 					this enumeration.
+		 */
 		public AlphaComposite getComposite()
 		{
 			return ac;
