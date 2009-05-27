6a460f2cb026853aa006a7ce42063849206f5a78
Chris Kroells <coobird@coobird.net>
2009-05-27 07:26:13 +0000

    Added note on considering usage of ExecutorCompletionService for concurrency wrapper.

--- src/net/coobird/paint/filter/ImageFilterThreadingWrapper.java
+++ src/net/coobird/paint/filter/ImageFilterThreadingWrapper.java
@@ -8,6 +8,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/* TODO Consider using ExecutorCompletionService
+ * Returns the completed tasks in a queue, and waits if none are ready.
+ * This may be able to reduce the wait time compared to the current
+ * implementation of waiting for the first Future.get to work.
+ * (Should allow better utilization of CPU.) 
+ * 
+ */
+
 /*
  * This wrapper only works if
  * BufferedImage(x,y,t) -> BufferedImage(x,y,t)
